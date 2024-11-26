cc804b0e52306554f6a4c6f7dfecc0ece15e4c9c
Julieth Daniela Rodríguez Manrique <juliethdanielarodriguezmanrique@Laptop-de-Julieth.local>
2024-11-26 07:27:22 -0500

    Proyecto base creado

---
+++ src/main/java/com/periferia/cliente/TerpelApplication.java
@@ -0,0 +1,13 @@
+package com.periferia.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerpelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerpelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/periferia/cliente/TerpelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.periferia.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerpelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
